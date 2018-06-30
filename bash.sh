52295f4bc4e45a491cb6810cdd6ea3039baa76c7
chenyongze <sapphire.php@gmail.com>
2018-06-30 11:08:41 +0800

    bash

--- bash.sh
+++ bash.sh
@@ -2,5 +2,5 @@
 docker stop `docker ps -a -q`
 docker rm -f `docker ps -a -q`
 docker rmi -f `docker images -q`
-docker build -t php-openresty ./
-docker run -it -d  -p 80:80 -p 443:443 -v /opt/docker/php-openresty/src:/usr/local/openresty/nginx/html --name sldc-php-openresty php-openresty
+docker build -t openresty-php7 ./
+docker run -it -d  -p 87:80 -p 1443:443 -v /opt/docker/php-openresty/src:/usr/local/openresty/nginx/html --name sldc-php-openresty openresty-php7
