6fd3f1f10f8bc024e7d101cb92e431700877e745
chenyongze <sapphire.php@gmail.com>
2018-06-30 09:49:21 +0800

    open 443 prot

--- bash.sh
+++ bash.sh
@@ -3,5 +3,4 @@ docker stop `docker ps -a -q`
 docker rm -f `docker ps -a -q`
 docker rmi -f `docker images -q`
 docker build -t php-openresty ./
-# docker run -it -p 80:80 php-openresty bash
-docker run -it -d  -p 80:80 -v /opt/docker/php-openresty/src:/usr/local/openresty/nginx/html --name sldc-php-openresty php-openresty
+docker run -it -d  -p 80:80 -p 443:443 -v /opt/docker/php-openresty/src:/usr/local/openresty/nginx/html --name sldc-php-openresty php-openresty
